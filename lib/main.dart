70d64bd0cc1cfa6ab22be7959319aaf026a62b4f
marscj <mjx_cj@163.com>
2021-01-14 20:36:21 +0400

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jpush_flutter/jpush_flutter.dart';
 import 'package:saadiyat/pages/app/app_bloc.dart';
 import 'package:saadiyat/pages/app/app_event.dart';
 import 'package:saadiyat/pages/app/app_page.dart';
