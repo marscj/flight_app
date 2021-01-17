d12a89825b84cae7dbb402d316f5c85a679df3fc
marscj <mjx_cj@163.com>
2021-01-17 18:12:02 +0400

    commit

--- lib/apis/client.g.dart
+++ lib/apis/client.g.dart
@@ -400,7 +400,7 @@ Map<String, dynamic> _$VersionToJson(Version instance) => <String, dynamic>{
 class _RestClient implements RestClient {
   _RestClient(this._dio, {this.baseUrl}) {
     ArgumentError.checkNotNull(_dio, '_dio');
-    baseUrl ??= 'http://192.168.1.248:8000/api/';
+    baseUrl ??= 'http://ubangservice.com/api/';
   }
 
   final Dio _dio;
